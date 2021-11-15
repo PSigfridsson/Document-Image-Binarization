4114e8a3e936c33082ddb29e3e90bd1146381a68
sleijon1 <simonleijon59@gmail.com>
2021-11-15 11:26:34 +0100

    fixed dibco script

--- utils/format_dibco.py
+++ utils/format_dibco.py
@@ -5,8 +5,10 @@ wd = os.getcwd()
 for year_folder in listdir(wd):
     if '.py' in year_folder:
         continue
-    sub_path = os.path.join(wd, year_folder)
-    for file in listdir(sub_path):
-        full_path = os.path.join(sub_path, file)
-        new_fp = os.path.join(sub_path, year_folder + "-" + file)
-        os.rename(full_path, new_fp)
+    year_path = os.path.join(wd, year_folder)
+    for og_gt in listdir(year_path):
+        sub_path = os.path.join(year_path, og_gt)
+        for file in listdir(sub_path):
+            full_path = os.path.join(sub_path, file)
+            new_fp = os.path.join(sub_path, year_folder + "-" + file)
+            os.rename(full_path, new_fp)
